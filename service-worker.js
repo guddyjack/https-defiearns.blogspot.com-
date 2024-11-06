d8294f2652937a360ddc601955e2c0b7543fce75
Ofonmbuk Jackson <ikonion50@gmail.com>
2024-11-06 14:19:32 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,39 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('offline-cache').then((cache) => {
+    caches.open('my-cache').then(cache => {
       return cache.addAll([
         '/',
-        '/index.html',
-        '/styles.css', // Include any critical CSS or JS files here
+        '/path/to/icon-self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/path/to/icon-192x192.png',
+        '/path/to/icon-512x512.png'
+        // Add other assets you want to cache here
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+',
+        '/path/to/icon-512x512.png'
+        // Add other assets you want to cache here
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
